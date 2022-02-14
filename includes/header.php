bc7efe259c1ee069e3bba91066d8dbd41152655a
Raul Morais <raulmoraiss47@gmail.com>
2022-02-14 14:09:36 -0300

    first commit

---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <title>RL Vagas!</title>
+  </head>
+  <body class="bg-dark text-light">
+
+    <div class="container">
+
+      <div class="jumbotron" style="background-color: orange; width: 100%;">
+        <h1>RL Vagas</h1>
+        <p class="text-black">CRUD com PHP orientados a objetos</p>
+      </div>
+
+
